389929c31131386fa2048e017323f37f076026df
Navneet Toppo <53862587+navneettoppo@users.noreply.github.com>
2024-03-06 20:24:59 +0530

    rds

--- 3tier/main.tf
+++ 3tier/main.tf
@@ -302,19 +302,21 @@ resource "aws_lb_listener" "external-elb" {
   }
 }
 
+
 resource "aws_db_instance" "default" {
   allocated_storage      = 10
   db_subnet_group_name   = aws_db_subnet_group.default.id
   engine                 = "mysql"
-  engine_version         = "8.0.20"
+  engine_version         = "8.0.28"  // updated to the latest version
   instance_class         = "db.t2.micro"
   multi_az               = true
   name                   = "mydb"
-  username               = "username"
-  password               = "password"
+  username               = "adminadmin"
+  password               = "adminadmin"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.database-sg.id]
 }
+
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
