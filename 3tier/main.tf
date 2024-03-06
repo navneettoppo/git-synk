a51174a582c8aa27421fad8a65f14103bb5d9dba
Navneet Toppo <53862587+navneettoppo@users.noreply.github.com>
2024-03-06 15:03:03 +0530

    update

--- 3tier/main.tf
+++ 3tier/main.tf
@@ -134,10 +134,6 @@ resource "aws_route_table_association" "b" {
 
 
 
-data "http" "example" {
-  url = "https://github.com/navneettoppo/git-synk/blob/main/3tier/install_apache2.sh"
-}
-
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
   ami                    = "ami-0d5eff06f840b45e9"
@@ -145,7 +141,15 @@ resource "aws_instance" "webserver1" {
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
-  user_data              = data.http.example.body
+  user_data              = <<-EOF
+                            #!/bin/bash
+
+                            yum update -y
+                            yum install -y httpd.x86_64
+                            systemctl start httpd.service
+                            systemctl enable httpd.service
+                            echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+                            EOF
 
   tags = {
     Name = "Web Server"
@@ -159,8 +163,15 @@ resource "aws_instance" "webserver2" {
   availability_zone      = "us-east-1b"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-2.id
-  user_data              = file("install_apache.sh")
+    user_data              = <<-EOF
+                            #!/bin/bash
 
+                            yum update -y
+                            yum install -y httpd.x86_64
+                            systemctl start httpd.service
+                            systemctl enable httpd.service
+                            echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+                            EOF
   tags = {
     Name = "Web Server"
   }
